1ac039394ee0114f1e85b1ac2515eb002cbb2936
luisosmx <119461863+luisosmx@users.noreply.github.com>
2023-03-25 17:59:29 -0600

    feat: add sum task

--- hello_world.py
+++ hello_world.py
@@ -6,10 +6,16 @@ from airflow.operators.python_operator import PythonOperator
 def print_hello():
     return 'Hello world! This worked!'
 
+def print_sum():
+    a = 5
+    b = 7
+    return f"{a} + {b} = {a+b}"
+
 dag = DAG('hello_world', description='Hello World DAG',
           schedule_interval='0 12 * * *',
           start_date=datetime(2017, 3, 20), catchup=False)
 
 hello_operator = PythonOperator(task_id='hello_task', python_callable=print_hello, dag=dag)
+sum_operator = PythonOperator(task_id='sum_task', python_callable=print_sum, dag=dag)
 
-hello_operator
+hello_operator >> sum_operator
