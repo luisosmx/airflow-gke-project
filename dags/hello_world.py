49a2320e461c454ddb3612e18ba079e91ac3fe98
luisosmx <luis.osmx16@gmail.com>
2023-03-25 19:23:29 -0600

    feat: multiplication task

--- dags/hello_world.py
+++ dags/hello_world.py
@@ -16,6 +16,11 @@ def print_division():
     b = 2
     return f"{a} / {b} = {a/b}"
 
+def print_multiplication():
+    a = 8
+    b = 5
+    return f"{a} * {b} = {a*b}"
+
 dag = DAG('hello_world', description='Hello World DAG',
           schedule_interval='0 12 * * *',
           start_date=datetime(2017, 3, 20), catchup=False)
@@ -23,5 +28,6 @@ dag = DAG('hello_world', description='Hello World DAG',
 hello_operator = PythonOperator(task_id='hello_task', python_callable=print_hello, dag=dag)
 sum_operator = PythonOperator(task_id='sum_task', python_callable=print_sum, dag=dag)
 division_operator = PythonOperator(task_id='division_task', python_callable=print_division, dag=dag)
+multiplication_operator = PythonOperator(task_id='multiplication_task', python_callable=print_multiplication, dag=dag)
 
-hello_operator >> sum_operator >> division_operator
+hello_operator >> sum_operator >> division_operator >> multiplication_operator
