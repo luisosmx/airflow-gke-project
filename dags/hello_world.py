a29bfbc7e7195218ec3a85343d5c0fc3e36dee01
luisosmx <119461863+luisosmx@users.noreply.github.com>
2023-03-25 18:40:47 -0600

    feat: add division

--- dags/hello_world.py
+++ dags/hello_world.py
@@ -11,11 +11,17 @@ def print_sum():
     b = 7
     return f"{a} + {b} = {a+b}"
 
+def print_division():
+    a = 10
+    b = 2
+    return f"{a} / {b} = {a/b}"
+
 dag = DAG('hello_world', description='Hello World DAG',
           schedule_interval='0 12 * * *',
           start_date=datetime(2017, 3, 20), catchup=False)
 
 hello_operator = PythonOperator(task_id='hello_task', python_callable=print_hello, dag=dag)
 sum_operator = PythonOperator(task_id='sum_task', python_callable=print_sum, dag=dag)
+division_operator = PythonOperator(task_id='division_task', python_callable=print_division, dag=dag)
 
-hello_operator >> sum_operator
+hello_operator >> sum_operator >> division_operator
